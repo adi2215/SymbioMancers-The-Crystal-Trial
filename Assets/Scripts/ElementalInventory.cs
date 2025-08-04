caf6a74ed5258eb75e0e984ba0fe9b7723856099
AnsarYesma <ansar.yesmukhanov@gmail.com>
2025-08-04 17:23:02 +0500

    Water+Bubble+Air&Water elements

--- Assets/Scripts/ElementalInventory.cs
+++ Assets/Scripts/ElementalInventory.cs
@@ -5,17 +5,73 @@ public class ElementalInventory : MonoBehaviour
 {
     public GameObject fireFollowerPrefab;
     public GameObject earthFollowerPrefab;
+    public GameObject airFollowerPrefab;
+    public GameObject waterFollowerPrefab;
     public GameObject burningSwordPrefab;
-
+    public GameObject bubbleShieldPrefab;
+    
     private Dictionary<ElementalType, GameObject> activeFollowers = new();
     private ElementalType? firstSelection = null;
-    private GameObject currentAbility;
+    private List<AbilityData> activeAbilities = new List<AbilityData>();
+
+    [System.Serializable]
+    private class AbilityData
+    {
+        public GameObject abilityObject;
+        public ElementalType firstElemental;
+        public ElementalType secondElemental;
+
+        public AbilityData(GameObject ability, ElementalType first, ElementalType second)
+        {
+            abilityObject = ability;
+            firstElemental = first;
+            secondElemental = second;
+        }
+    }
+
+    [System.Serializable]
+    private class ElementalCombination
+    {
+        public ElementalType firstElemental;
+        public ElementalType secondElemental;
+        public GameObject abilityPrefab;
+        public string combinationName;
+
+        public ElementalCombination(ElementalType first, ElementalType second, GameObject prefab, string name)
+        {
+            firstElemental = first;
+            secondElemental = second;
+            abilityPrefab = prefab;
+            combinationName = name;
+        }
+    }
+
+    private List<ElementalCombination> possibleCombinations;
+
+    void Awake()
+    {
+        InitializeCombinations();
+    }
+
+    void InitializeCombinations()
+    {
+        possibleCombinations = new List<ElementalCombination>
+        {
+            new ElementalCombination(ElementalType.Fire, ElementalType.Earth, burningSwordPrefab, "Burning Sword"),
+            new ElementalCombination(ElementalType.Air, ElementalType.Water, bubbleShieldPrefab, "Bubble Shield")
+            // Add new combinations here easily
+        };
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) SelectElemental(ElementalType.Fire);
         if (Input.GetKeyDown(KeyCode.Alpha2)) SelectElemental(ElementalType.Earth);
-        if (Input.GetKeyDown(KeyCode.Q)) DischargeAbility();
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SelectElemental(ElementalType.Air);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) SelectElemental(ElementalType.Water);
+        
+        // Q discharges all abilities
+        if (Input.GetKeyDown(KeyCode.Q)) DischargeAllAbilities();
     }
 
     public bool TryAddElemental(ElementalType type)
@@ -50,39 +106,56 @@ public class ElementalInventory : MonoBehaviour
 
     void CombineElementals(ElementalType a, ElementalType b)
     {
-        bool ok = false;
-        if ((a == ElementalType.Fire && b == ElementalType.Earth) || (a == ElementalType.Earth && b == ElementalType.Fire))
+        ElementalCombination combination = FindCombination(a, b);
+        
+        if (combination != null)
         {
-            currentAbility = Instantiate(burningSwordPrefab, transform.position, Quaternion.identity);
-            currentAbility.transform.SetParent(transform);
-            ok = true;
-        }
-        else
-        {
-            Debug.Log($"No ability for combination: {a} + {b}");
-            ok = false;
-        }
-
-        if (ok)
-        {
+            GameObject ability = Instantiate(combination.abilityPrefab, transform.position, Quaternion.identity);
+            ability.transform.SetParent(transform);
+            
+            AbilityData abilityData = new AbilityData(ability, combination.firstElemental, combination.secondElemental);
+            activeAbilities.Add(abilityData);
+            
             Destroy(activeFollowers[a]);
             Destroy(activeFollowers[b]);
             activeFollowers.Remove(a);
             activeFollowers.Remove(b);
+            
+            Debug.Log($"Created {combination.combinationName}!");
+        }
+        else
+        {
+            Debug.Log($"No ability for combination: {a} + {b}");
         }
     }
 
-    void DischargeAbility()
+    ElementalCombination FindCombination(ElementalType a, ElementalType b)
     {
-        if (currentAbility != null)
+        foreach (var combination in possibleCombinations)
         {
-            Destroy(currentAbility);
-            currentAbility = null;
-
-            // Restore elementals
-            TryAddElemental(ElementalType.Fire);
-            TryAddElemental(ElementalType.Earth);
+            if ((combination.firstElemental == a && combination.secondElemental == b) ||
+                (combination.firstElemental == b && combination.secondElemental == a))
+            {
+                return combination;
+            }
         }
+        return null;
+    }
+
+    void DischargeAllAbilities()
+    {
+        if (activeAbilities.Count == 0) return;
+
+        // Restore all elementals from all abilities
+        foreach (var abilityData in activeAbilities)
+        {
+            TryAddElemental(abilityData.firstElemental);
+            TryAddElemental(abilityData.secondElemental);
+            Destroy(abilityData.abilityObject);
+        }
+        
+        activeAbilities.Clear();
+        Debug.Log("All abilities discharged!");
     }
 
     GameObject GetPrefab(ElementalType type)
@@ -91,6 +164,8 @@ public class ElementalInventory : MonoBehaviour
         {
             ElementalType.Fire => fireFollowerPrefab,
             ElementalType.Earth => earthFollowerPrefab,
+            ElementalType.Air => airFollowerPrefab,
+            ElementalType.Water => waterFollowerPrefab,
             _ => null
         };
     }
@@ -101,6 +176,8 @@ public class ElementalInventory : MonoBehaviour
         {
             ElementalType.Fire => new Vector3(1, 1, 0),
             ElementalType.Earth => new Vector3(-1, 1, 0),
+            ElementalType.Air => new Vector3(1, -1, 0),
+            ElementalType.Water => new Vector3(-1, -1, 0),
             _ => Vector3.zero
         };
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,24 +9,114 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.1f;
 
+    [Header("Water Settings")]
+    [SerializeField] private float waterMoveSpeedMultiplier = 0.5f;
+    [SerializeField] private float waterGravityMultiplier = 0.3f;
+    [SerializeField] private float waterExitJumpForce = 10f;
+
+    [Header("Swim Mode Settings")]
+    [SerializeField] private bool _isInSwimMode = false;
+    [SerializeField] private float _swimSpeed = 3f;
+    [SerializeField] private float _swimGravityScale = 0.7f;
+
     private Rigidbody2D _rb;
     [SerializeField] private bool _isGrounded;
+    [SerializeField] private bool _isInWater;
+    private float _originalGravityScale;
+    private float _originalMoveSpeed;
+    private BubbleShield _activeBubbleShield;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _originalGravityScale = 1f;
+        _originalMoveSpeed = moveSpeed;
     }
 
     private void Update()
     {
+        if (_isInSwimMode)
+        {
+            HandleSwimMovement();
+        }
+        else
+        {
+            HandleNormalMovement();
+        }
+    }
+
+    private void HandleNormalMovement()
+    {
+        float currentMoveSpeed = _isInWater ? moveSpeed * waterMoveSpeedMultiplier : moveSpeed;
         float moveX = Input.GetAxisRaw("Horizontal");
-        _rb.linearVelocity = new Vector2(moveX * moveSpeed, _rb.linearVelocity.y);
+        _rb.linearVelocity = new Vector2(moveX * currentMoveSpeed, _rb.linearVelocity.y);
 
         _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        // Only allow jumping when grounded and not in water
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded && !_isInWater)
         {
             _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void HandleSwimMovement()
+    {
+        // Four-directional swimming movement
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        
+        Vector2 swimVelocity = new Vector2(moveX * _swimSpeed, moveY * _swimSpeed);
+        _rb.linearVelocity = swimVelocity;
+    }
+
+    public void EnableSwimMode(float swimSpeed, float swimGravityScale)
+    {
+        _isInSwimMode = true;
+        _swimSpeed = swimSpeed;
+        _swimGravityScale = swimGravityScale;
+        _rb.gravityScale = swimGravityScale;
+        
+        // Find active bubble shield
+        _activeBubbleShield = FindObjectOfType<BubbleShield>();
+    }
+
+    public void DisableSwimMode()
+    {
+        _isInSwimMode = false;
+        _rb.gravityScale = _originalGravityScale;
+        _activeBubbleShield = null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            _isInWater = true;
+            if (!_isInSwimMode)
+            {
+                _rb.gravityScale = waterGravityMultiplier;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            _isInWater = false;
+            
+            // If in swim mode, destroy bubble shield when exiting water
+            if (_isInSwimMode && _activeBubbleShield != null)
+            {
+                _activeBubbleShield.OnPlayerExitWater();
+                // Add jump effect when exiting water to simulate jumping out
+                Vector2 currentVelocity = _rb.linearVelocity;
+                _rb.linearVelocity = new Vector2(currentVelocity.x, waterExitJumpForce);
+            }
+            
+            _rb.gravityScale = 1f;
+            
         }
     }
 }
